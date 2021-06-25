6aa5c54753162bf0e672c8d737253e3171290314
RubenSmn <voetbalruben@gmail.com>
2021-06-25 20:21:59 +0200

    Add timestamps to models

--- backend/todoapp/models.py
+++ backend/todoapp/models.py
@@ -1,7 +1,8 @@
 from enum import unique
 from playhouse.shortcuts import model_to_dict
 from todoapp import db
-from peewee import BooleanField, ForeignKeyField, Model, CharField
+from peewee import BooleanField, DateTimeField, ForeignKeyField, Model, CharField
+import datetime
 
 class BaseModel(Model):
   class Meta:
@@ -12,8 +13,12 @@ class BaseModel(Model):
 
 class Category(BaseModel):
   category = CharField(unique=True)
+  created_at = DateTimeField(deafult=datetime.datetime.now)
 
 class Todo(BaseModel):
   title = CharField()
   completed = BooleanField()
   category = ForeignKeyField(Category, backref='todos')
+  deleted = BooleanField(default=False)
+  created_at = DateTimeField(deafult=datetime.datetime.now)
+  updated_at = DateTimeField(deafult=datetime.datetime.now)
